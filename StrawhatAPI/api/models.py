b4ec9ed238b358fe2ca86d922d62ecae50a971ab
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2022-10-01 08:42:25 +0200

    feature: Added Recidence model

--- StrawhatAPI/api/models.py
+++ StrawhatAPI/api/models.py
@@ -4,11 +4,31 @@ class OnePieceCharacter(db.Model):
     name = db.Column(db.String(255))
     crew = db.Column(db.String(255))
     devilfruit = db.Column(db.String(255))
+    residence = db.Column(db.String(255))
+    bounty = db.Column(db.Integer)
 
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
             "crew": self.crew,
-            "devilfruit": self.devilfruit
+            "devilfruit": self.devilfruit,
+            "residence": self.residence,
+            "bounty": self.bounty
+        }
+
+class Residence(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    region = db.Column(db.String(255))
+    kingdom = db.Column(db.String(255))
+    type = db.Column(db.String(255))
+    population = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "region": self.region,
+            "kingdom": self.kingdom,
+            "type": self.type,
+            "population": self.population            
         }
--- StrawhatAPI/api/mutations.py
+++ StrawhatAPI/api/mutations.py
@@ -4,12 +4,14 @@ from api.models import OnePieceCharacter
 from ariadne import convert_kwargs_to_snake_case
 
 
-def create_character_resolver(obj, info, devilfruit, name, crew):
+def create_character_resolver(obj, info, name, crew,  devilfruit, bounty):
     try:
         print(devilfruit)
         character = OnePieceCharacter(name=name,
                                       crew=crew,
-                                      devilfruit=devilfruit)
+                                      devilfruit=devilfruit,
+                                      bounty = bounty
+                                      )
         db.session.add(character)
         db.session.commit()
         payload = {"success": True, "character": character}
@@ -25,13 +27,14 @@ def create_character_resolver(obj, info, devilfruit, name, crew):
     return payload
 
 @convert_kwargs_to_snake_case
-def update_character_resolver(obj, info, id, devilfruit, name, crew):
+def update_character_resolver(obj, info, id, name, crew,  devilfruit, bounty):
     try:
         character = OnePieceCharacter.query.get(id)
         if character:
             character.name = name
-            character.devilfruit = devilfruit
             character.crew = crew
+            character.devilfruit = devilfruit
+            character.bounty = bounty
         db.session.add(character)
         db.session.commit()
         payload = {
