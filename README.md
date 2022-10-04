e396b57ad138f75ea184cd3b58af26c49515f9d7
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2022-10-04 10:58:56 +0200

    Feature: Models are imported into the SQL database. Written part of the Readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # StrawHat_API_Django_Python
+
+![](https://upload.wikimedia.org/wikipedia/en/thumb/2/2c/One_Piece_Logo.svg/800px-One_Piece_Logo.svg.png)
+
+My favorite thing in the world is sitting in my pajama's and watching One Piece on Sunday morning.
+That's why I decided to make this API, to gain more knowledge about programming in Python and also 
+gain more knowledge about the incredible world Oda has created!
+
+
--- StrawhatAPI/api/models.py
+++ StrawhatAPI/api/models.py
@@ -5,21 +5,9 @@ from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import case
 #https://docs.sqlalchemy.org/en/14/orm/basic_relationships.html#one-to-one
 
-# class Parent(Base):
-#     __tablename__ = "parent"
-#     id = Column(Integer, primary_key=True)
-#
-#     # one-to-many collection
-#     children = relationship("Child", back_populates="parent")
-#
-#
-# class Child(Base):
-#     __tablename__ = "child"
-#     id = Column(Integer, primary_key=True)
-#     parent_id = Column(Integer, ForeignKey("parent.id"))
-#
-#     # many-to-one scalar
-#     parent = relationship("Parent", back_populates="children")
+# origin = db.Column(db.String(255), db.ForeignKey('origin.id'))
+    # crew = db.Column(db.String(255), db.ForeignKey('crew.id'))
+    # piratefleet = db.Column(db.String(255), db.ForeignKey('crew.id'))
 class OnePieceCharacter(db.Model):
     __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
@@ -28,15 +16,13 @@ class OnePieceCharacter(db.Model):
     occupation = db.Column(db.String(255))
     nickName = db.Column(db.String(255))
     devilfruit = db.Column(db.String(255))
-    origin = db.Column(db.String(255), db.ForeignKey('origin.id'))
-    crew = db.Column(db.String(255), db.ForeignKey('crew.id'))
-    piratefleet = db.Column(db.String(255), db.ForeignKey('crew.id'))
+    isAlive = db.Column(db.Boolean)
+    hasDevilFruit = db.Column(db.Boolean)
+    isPartOfFleet = db.Column(db.Boolean)
     bounty = db.Column(db.Integer)
-    # Relationships (?)
-    originRelationship = relationship("Origin", back_populates="OnePieceCharacter")
-    crewRelationship = relationship("Crew", back_populates="OnePieceCharacter")
-    piratefleetRelationship = relationship("PirateFleet", back_populates="OnePieceCharacter")
-    DevilFruitRelationship = relationship("DevilFruit", back_populates="OnePieceCharacter")
+    age = db.Column(db.Integer)
+    height = db.Column(db.Integer)
+    birthday = db.Column(db.Date)
 
     def to_dict(self):
         return {
@@ -58,7 +44,7 @@ class Origin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     hasKingdom = db.Column(Boolean, unique=False, default=True)
-    parent = relationship("OnePieceCharacter", back_populates="Origin")
+    # parent = relationship("OnePieceCharacter", back_populates="Origin")
 
     def to_dict(self):
         return {
@@ -66,7 +52,6 @@ class Origin(db.Model):
             "name": self.name,
             "haskingdom": self.hasKingdom
         }
-
 #Crew
 
 class Crew(db.Model):
@@ -77,7 +62,7 @@ class Crew(db.Model):
     captain = db.Column(db.String(255))
     mainShip = db.Column(db.String(255))
     totalBounty = db.Column(db.Integer)
-    parent = relationship("OnePieceCharacter", back_populates="Crew")
+
 
     def to_dict(self):
         return {
@@ -98,7 +83,7 @@ class PirateFleet(db.Model):
     captain = db.Column(db.String(255))
     totalPeople = db.Column(db.Integer)
     totalBounty = db.Column(db.Integer)
-    parent = relationship("OnePieceCharacter", back_populates="Origin")
+
 
     def to_dict(self):
         return {
@@ -138,5 +123,5 @@ class DevilFruitTypes(db.Model):
     __devilfruittype__ = 'devilfruittype'
     id = db.Column(Integer, primary_key=True)
     value = db.Column(Enum("paramecia", "zoan", "logia", name="ValueTypes"), default = "zoan")
-    originRelationship = relationship("DevilFruit", back_populates="devilfruittype")
+
 
