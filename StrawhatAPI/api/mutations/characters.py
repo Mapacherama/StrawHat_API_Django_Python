6c1efcdfa28e32e56bdfe7fe17a5f142cba9f486
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2022-10-16 11:08:47 +0200

    Feature: Character Boolean values can be updated

--- StrawhatAPI/api/mutations/characters.py
+++ StrawhatAPI/api/mutations/characters.py
@@ -13,19 +13,20 @@ def create_character_resolver(obj, _info, **kwargs):
 
     return create_result(character)
 
-@convert_kwargs_to_snake_case
+
 def update_character_resolver(obj, _info, **kwargs):
     errors = []
     character = OnePieceCharacter.query.get(kwargs["id"])
+    print(character.isalive)
     print(kwargs)
     if not character:
         return create_result(status=False, errors=[Errors.OBJECT_NOT_FOUND])
     #Look at the variable names of the dictionary ea in the kwargs dictionary.
-    if kwargs.get("has_devil_fruit"):
-        character.hasdevilFruit = kwargs["has_devil_fruit"]
+    if kwargs.get("hasdevil_fruit"):
+        character.hasdevilFruit = kwargs["hasdevil_fruit"]
 
-    if kwargs.get("is_part_of_fleet"):
-        character.ispartOffleet = kwargs["is_part_of_fleet"]
+    if kwargs.get("ispart_offleet"):
+        character.ispartOffleet= kwargs["ispart_offleet"]
 
     if kwargs.get("is_alive"):
         character.isalive = kwargs["is_alive"]
@@ -33,7 +34,7 @@ def update_character_resolver(obj, _info, **kwargs):
     character.update(**kwargs)
     db.session.commit()
 
-    return create_result(character = character)
+    return create_result(errors= errors, character = character)
 
 @convert_kwargs_to_snake_case
 def delete_character_resolver(obj, info, **kwargs):
