429be7766938e0dbd4f392448663f2afddad55de
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2022-10-23 15:53:35 +0200

    Test: Started on the unit tests

--- StrawhatAPI/test/test_app.py
+++ StrawhatAPI/test/test_app.py
@@ -16,3 +16,9 @@ query_for_single_pirate = """query{
 def test_retrieve_graphql_data_should_yield_http_200():
     response = requests.post(GRAPHQL_API_ENDPOINT, json={'query': query_for_single_pirate})
     assert response.status_code == 200
+
+def test_retrieve_character_name_should_be_correct():
+    response = requests.post(GRAPHQL_API_ENDPOINT, json={'query': query_for_single_pirate})
+
+    response_body = response.json()
+    assert response_body['data']['getSingleCharacter']['character']['name'] == 'Monkey D. Luffy'
