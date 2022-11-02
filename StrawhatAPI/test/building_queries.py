2601b1d4b2015ac991bf0036cf2a9d8a7061f0a7
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2022-11-02 09:20:47 +0100

    Testing: Started implementing second test method

---
+++ StrawhatAPI/test/building_queries.py
@@ -0,0 +1,9 @@
+import requests
+
+from sgqlc.types import String, Type, Field, Boolean
+from sgqlc.operation import Operation
+
+class originType(Type):
+    id = String
+    name = String
+    description = String
